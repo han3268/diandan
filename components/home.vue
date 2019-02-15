11018bc9511b33f539e2fe78c31b77b67cbea074
han3268 <han3268@outlook.com>
2019-02-15 09:47:34 +0800

    初始化仓库

---
+++ components/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+      msg:"脚手架1111111111",
+        
+    }},
+    methods:{
+
+
+    },
+    beforeCreate(){
+      //创建组件实例之前触发
+    },
+    created(){
+      //创建组件实例之后，但挂this.show()
+    }
+  }
+</script>
+
+<style >
+
+</style>
